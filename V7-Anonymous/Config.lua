dd3220029b32562d93a3aab86440853417592835
V7-Store <137654968+V7-Store@users.noreply.github.com>
2023-06-25 21:18:13 +0300

    Add files via upload

---
+++ V7-Anonymous/Config.lua
@@ -0,0 +1,22 @@
+Config = {}
+------------------------------------------------------  V7-ScriptsName  --------------------------------------
+Config.core = 'FC-core' -- اسم الكور حقك
+
+Config.target = 'FC-target' -- اسم العين او التارقت حقك
+
+Config.menu = "FC-menu" -- اسم سكربت المنيو حقك
+
+Config.input = "V7-Input" -- اسم الانبت حقك
+------------------------------------------------------  V7-cams  ---------------------------------------------
+Config.SecurityCameras = {
+    hideradar = false,
+    cameras = {
+        [1] = {label = "Pacific Bank CAM#1", coords = vector3(-301.83, -1367.53, 34.33), r = {x = -25.0, y = 0.0, z = -38.05}, canRotate = false, isOnline = true}, --لاتلعب فيهم
+        [2] = {label = "Pacific Bank CAM#2", coords = vector3(-295.38, -1348.66, 35.58), r = {x = -25.0, y = 0.0, z = -140.91}, canRotate = false, isOnline = true}, --لاتلعب فيهم
+        [3] = {label = "Pacific Bank CAM#3", coords = vector3(-316.76, -1368.89, 34.36), r = {x = -35.0, y = 0.0, z = 34.87}, canRotate = false, isOnline = true}, --لاتلعب فيهم
+        [4] = {label = "Limited Ltd Grove St. CAM#1", coords = vector3(-328.23, -1368.93, 32.59), r = {x = -15.0, y = 0.0, z = -380.91}, canRotate = false, isOnline = true}, --لاتلعب فيهم
+        [5] = {label = "Rob's Liqour Prosperity St. CAM#1", coords = vector3(-298.5, -1390.29, 35.68), r = {x = -35.0, y = 0.0, z = 79.53281}, canRotate = false, isOnline = true}, --لاتلعب فيهم
+        
+    },
+}
+--------------------------------------------------------------------------------------------------------------
